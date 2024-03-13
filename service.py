84b0881d694355ca1fce9112a4c9d95ccf7e272d
New Bing <iflamed@gmail.com>
2024-03-13 23:58:07 +0800

    Update service.py to add img2img api

--- service.py
+++ service.py
@@ -2,11 +2,15 @@ import bentoml
 from PIL.Image import Image
 from annotated_types import Le, Ge
 from typing_extensions import Annotated
-
+from diffusers.utils import load_image, make_image_grid
+from diffusers import AutoPipelineForText2Image
+from diffusers import AutoPipelineForImage2Image
+import torch
 
 MODEL_ID = "stabilityai/sdxl-turbo"
 
 sample_prompt = "A cinematic shot of a baby racoon wearing an intricate italian priest robe."
+sample_image = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/cat.png"
 
 @bentoml.service(
     traffic={"timeout": 300},
@@ -18,15 +22,13 @@ sample_prompt = "A cinematic shot of a baby racoon wearing an intricate italian 
 )
 class SDXLTurbo:
     def __init__(self) -> None:
-        from diffusers import AutoPipelineForText2Image
-        import torch
-
         self.pipe = AutoPipelineForText2Image.from_pretrained(
             MODEL_ID,
             torch_dtype=torch.float16,
             variant="fp16",
         )
         self.pipe.to(device="cuda")
+        self.pipeline = AutoPipelineForImage2Image.from_pipe(self.pipe).to("cuda")
 
     @bentoml.api
     def txt2img(
@@ -39,5 +41,26 @@ class SDXLTurbo:
             prompt=prompt,
             num_inference_steps=num_inference_steps,
             guidance_scale=guidance_scale,
+        ).images[0]
+        return image
+
+    @bentoml.api
+    def img2img(
+            self,
+            prompt: str = sample_prompt,
+            image_url: str = sample_image,
+            num_inference_steps: Annotated[int, Ge(1), Le(10)] = 2,
+            guidance_scale: float = 0.0,
+            strength: float = 0.5,
+    ) -> Image:
+        init_image = load_image(image_url)
+        init_image = init_image.resize((512, 512))
+
+        image = self.pipeline(
+            prompt,
+            image=init_image,
+            strength=strength,
+            guidance_scale=guidance_scale,
+            num_inference_steps=num_inference_steps,
         ).images[0]
         return image
